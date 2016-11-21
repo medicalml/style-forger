dbf75da68bf23921a1e2e986580ede05767dca45
Bogusz Stefańczyk <stefanczykb@gmail.com>
2016-11-21 20:25:13 +0100

    Revert "refactored mock initialization in main"
    This reverts commit 846e0798cb87a6ac5d7c3eeb4852289baa981102.

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ import Mocks
 from facebook_upload import fb
 from chainer_fast_neuralstyle.TransformationApplier import TransformationApplier
 
+if config.MOCK_TRANSFORMATION_APPLIER:
+    transformationApplier = Mocks.TransformationApplierMock(1)
+else:
+    transformationApplier = TransformationApplier()
 
-transformationApplier = Mocks.TransformationApplierMock(1) if config.MOCK_TRANSFORMATION_APPLIER \
-                        else TransformationApplier()
-
-afterTransformationAction = Mocks.afterTransformationActionMock if config.MOCK_FACEBOOK_UPLOAD \
-                            else afterTransformationAction = fb.upload_file
+if config.MOCK_FACEBOOK_UPLOAD:
+    afterTransformationAction = Mocks.afterTransformationActionMock
+else:
+    afterTransformationAction = fb.upload_file
 
 cameraImageProvider = CameraImageProvider()
 transformationProvider = TransformationProvider(cameraImageProvider, transformationApplier)
