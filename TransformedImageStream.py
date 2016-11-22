978e4f40aa5fe896762824bf1542c729b6b29218
Bogusz Stefańczyk <stefanczykb@gmail.com>
2016-11-22 23:41:54 +0100

    changed TransformedImageStream to non-singleton

--- TransformedImageStream.py
+++ TransformedImageStream.py
@@ -4,10 +4,7 @@ import config
 
 
 class TransformedImageStream:
-    instance = None  # has to be a singleton, TkInter key listeners only accept static mathods
-
     def __init__(self, root, cameraImageStream, transformationApplier):
-        TransformedImageStream.instance = self
         self.cameraImageStream = cameraImageStream
         self.transformationApplier = transformationApplier
         self.transformationDeleter = DelayedTask(root, config.TRANSFORMATION_DISPLAY_TIME, self.forgetTransformation)
@@ -18,10 +15,7 @@ class TransformedImageStream:
         self.updateTransformation()
         return self.transformedFrame
 
-    @staticmethod
-    def initiateFrameTransformation(event):
-        self = TransformedImageStream.instance
-        assert self is not None
+    def initiateFrameTransformation(self, event):
         if self.isShowingTransformation() and not self.isTransformationProcessing():
             self.transformCurrentFrame()
 
