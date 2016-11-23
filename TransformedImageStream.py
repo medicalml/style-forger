115e1778d1f32277df29b001784fdd6ad3a73462
froger <you@example.com>
2016-11-23 21:51:48 +0100

    after transformation action quick fix

--- TransformedImageStream.py
+++ TransformedImageStream.py
@@ -2,6 +2,9 @@ from AsyncTransformator import AsyncTransformator
 from helpers import DelayedTask
 import config
 
+from facebook_upload import fb
+import Mocks
+
 
 class TransformedImageStream:
     def __init__(self, root, cameraImageStream, transformationApplier):
@@ -31,4 +34,11 @@ class TransformedImageStream:
 
     def updateTransformation(self):
         if self.asyncTransformator.hasAwaitingResult():
+            afterTransformationAction = None
+            if config.MOCK_FACEBOOK_UPLOAD:
+                afterTransformationAction = Mocks.afterTransformationActionMock
+            else:
+                afterTransformationAction = fb.upload_file
             self.transformedFrame = self.asyncTransformator.getTransformedFrame()
+            afterTransformationAction(self.transformedFrame.copy())
+
