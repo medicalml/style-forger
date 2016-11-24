98afce5694977cceb63b0f35e4706e0505007b6e
Bogusz Stefańczyk <stefanczykb@gmail.com>
2016-11-24 22:57:50 +0100

    moved initialization from main to initialization.py

--- Application.py
+++ Application.py
@@ -1,20 +1,22 @@
 from display.FullscreenCanvas import FullscreenCanvas
 from display.PulsingCircle import PulsingCircle
 from display.ImageStreamDisplay import ImageStreamDisplay
+from initialization import initializeImageStreams
 
 
 class Application(object):
-    def __init__(self, root, cameraImageStream, transformedImageStream):
+    def __init__(self, root):
         self.root = root
-        windowSize = getWindowSize(root)
+        windowSize = _getWindowSize(root)
         self.canvas = FullscreenCanvas(root, windowSize)
 
+        cameraImageStream, transformedImageStream = initializeImageStreams(root)
         self.canvas.addDrawableChild(ImageStreamDisplay(self.canvas, cameraImageStream, windowSize))
         self.canvas.addDrawableChild(ImageStreamDisplay(self.canvas, transformedImageStream, windowSize))
         #self.canvas.addDrawableChild(PulsingCircle(self.canvas, (50,50)))
 
         root.bind('<Return>', transformedImageStream.initiateFrameTransformation)
 
-def getWindowSize(root):
+def _getWindowSize(root):
     root.update()
     return root.winfo_width(), root.winfo_height()
--- main.py
+++ main.py
@@ -1,27 +1,8 @@
 import Tkinter as tk
 from Application import Application
-from CameraImageStream import CameraImageStream
-from TransformedImageStream import TransformedImageStream
-import config
-import Mocks
-from facebook_upload import fb
-from chainer_fast_neuralstyle.TransformationApplier import TransformationApplier
 
 root = tk.Tk()
 root.attributes("-fullscreen",True)
 
-if config.MOCK_TRANSFORMATION_APPLIER:
-    transformationApplier = Mocks.TransformationApplierMock(1)
-else:
-    transformationApplier = TransformationApplier()
-
-if config.MOCK_FACEBOOK_UPLOAD:
-    afterTransformationAction = Mocks.afterTransformationActionMock
-else:
-    afterTransformationAction = fb.upload_file
-
-cameraImageStream = CameraImageStream()
-transformedImageStream = TransformedImageStream(root, cameraImageStream, transformationApplier, afterTransformationAction)
-
-app = Application(root, cameraImageStream, transformedImageStream)
+app = Application(root)
 root.mainloop()
