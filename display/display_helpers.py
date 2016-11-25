a7f39c593eda439c03181d3fdecd6ee500f536a1
Bogusz Stefańczyk <stefanczykb@gmail.com>
2016-11-25 17:45:16 +0100

    fixed boundingBoxSquare

--- display/display_helpers.py
+++ display/display_helpers.py
@@ -4,11 +4,12 @@ import time
 
 def boundingBoxSquare(center, size):
     x, y = center
+    halfSize = size/2
     return (
-        x-size, # x of top-right corner
-        y-size,  # y of top-right corner
-        x+size, # x of top-right corner
-        y+size)  # y of top-right corner
+        x-halfSize, # x of top-left corner
+        y-halfSize,  # y of top-left corner
+        x+halfSize, # x of bottom-right corner
+        y+halfSize)  # y of bottom-right corner
 
 # According to timeit.timeit sourcecode:
 # On Windows, the best timer is time.clock
