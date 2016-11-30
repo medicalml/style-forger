51ef994e7780fd0a9f79b680e49214b269b5d016
froger <froger@gmail.com>
2016-11-30 18:53:42 +0100

    added median filter to chainger_fast_neuralstyle/generate

--- chainer_fast_neuralstyle/source/generate.py
+++ chainer_fast_neuralstyle/source/generate.py
@@ -1,9 +1,11 @@
 from __future__ import print_function
 import time
+
+from PIL import Image, ImageFilter
 from chainer import cuda, serializers
 from net import *
 
-def generate(model, _imageArray, _gpu = -1, _padding=50):
+def generate(model, _imageArray, _gpu = -1, _padding=50, _medianFilter=3):
     xp = np if _gpu < 0 else cuda.cupy
     
     start = time.time()
@@ -21,5 +23,11 @@ def generate(model, _imageArray, _gpu = -1, _padding=50):
     if _padding > 0:
         result = result[:, :, _padding:-_padding, _padding:-_padding]
     result = np.uint8(result[0].transpose((1, 2, 0)))
+
+    if _medianFilter > 0:
+        pilImage = Image.fromarray(result)
+        pilImage = pilImage.filter(ImageFilter.MedianFilter(_medianFilter))
+        result = np.array(pilImage, dtype=np.uint8)
+
     print('Transforming took ', time.time() - start, 'sec')
     return result
